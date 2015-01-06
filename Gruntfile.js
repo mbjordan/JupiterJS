150f323e591f0a0dcb19e56fa763088c23b4e534
Matt Jordan <mbj755@gmail.com>
2015-01-06 15:24:45 -0500

    fixed some broken shit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,31 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        jasmine_node: {
-            task_name: {
-                options: {
-                    coverage: {},
-                    forceExit: true,
-                    match: '.',
-                    matchAll: false,
-                    specFolders: ['spec'],
-                    extensions: 'js',
-                    specNameMatcher: 'spec',
-                    captureExceptions: true,
-                    junitreport: {
-                        report: false
-                    }
-                },
-                src: ['jupiter.js']
-            }
-        },
-        coveralls: {
-            options: {
-                src: 'coverage/lcov.info',
-                force: true
-            }
-        }
-    });
+	grunt.initConfig({
+		jasmine_node: {
+			task_name: {
+				options: {
+					coverage: {},
+					forceExit: true,
+					match: '.',
+					matchAll: false,
+					specFolders: ['spec'],
+					extensions: 'js',
+					specNameMatcher: 'spec',
+					captureExceptions: true,
+					junitreport: {
+						report: false
+					}
+				},
+				src: ['jupiter.js']
+			}
+		},
+		coveralls: {
+			src: 'coverage/lcov.info'
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
-    grunt.loadNpmTasks('grunt-coveralls');
+	grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+	grunt.loadNpmTasks('grunt-coveralls');
 
-    grunt.registerTask('test', ['jasmine_node', 'coveralls']);
+	grunt.registerTask('test', ['jasmine_node', 'coveralls']);
 };
