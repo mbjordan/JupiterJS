04c447ed106726bb139a4ce909e58a5974c65b74
Matt Jordan <mbj755@gmail.com>
2015-01-06 14:50:34 -0500

    updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,27 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		jasmine_node: {
-			task_name: {
-				options: {
-					coverage: {},
-					forceExit: true,
-					match: '.',
-					matchAll: false,
-					specFolders: ['spec'],
-					extensions: 'js',
-					specNameMatcher: 'spec',
-					captureExceptions: true,
-					junitreport: {
-						report: false
-					}
-				},
-				src: ['*.js']
-			}
-		}
-	});
+    grunt.initConfig({
+        jasmine_node: {
+            task_name: {
+                options: {
+                    coverage: {},
+                    forceExit: true,
+                    match: '.',
+                    matchAll: false,
+                    specFolders: ['spec'],
+                    extensions: 'js',
+                    specNameMatcher: 'spec',
+                    captureExceptions: true,
+                    junitreport: {
+                        report: false
+                    }
+                },
+                src: ['jupiter.js']
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
 
-	grunt.registerTask('test', 'jasmine_node');
+    grunt.registerTask('test', 'jasmine_node');
 };
