7a962affb73940d94258e44738272ff68d9399b7
Matt Jordan <mbj755@gmail.com>
2015-01-06 22:56:51 -0500

    grunt change

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        jasmine_node: {
-            jupiter_test: {
-                options: {
-                    coverage: {
-                        reportFile: 'coverage.json',
-                        savePath: 'coverage',
-                        report: ['lcov']
-                    },
-                    forceExit: true,
-                    match: '.',
-                    matchAll: false,
-                    specFolders: ['spec'],
-                    extensions: 'js',
-                    specNameMatcher: 'spec',
-                    captureExceptions: true,
-                    junitreport: {
-                        report: false
-                    }
-                },
-                src: ['jupiter.js']
-            }
-        },
-        coveralls: {
-            jupiter_test: {
-                src: 'coverage/lcov.info',
-                force: true
-            }
-        }
-    });
+	grunt.initConfig({
+		jasmine_node: {
+			jupiter_test: {
+				options: {
+					coverage: {
+						reportFile: 'coverage.json',
+						savePath: 'coverage',
+						report: ['lcov']
+					},
+					forceExit: true,
+					match: '.',
+					matchAll: false,
+					specFolders: ['spec'],
+					extensions: 'js',
+					specNameMatcher: 'spec',
+					captureExceptions: true,
+					junitreport: {
+						report: false
+					}
+				},
+				src: ['jupiter.js']
+			}
+		},
+		coveralls: {
+			jupiter_test: {
+				src: 'coverage/lcov.info',
+				force: true
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
-    grunt.loadNpmTasks('grunt-coveralls');
+	grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+	grunt.loadNpmTasks('grunt-coveralls');
 
-    grunt.registerTask('jastest', 'jasmine_node:jupiter_test');
-    grunt.registerTask('coverload', 'coveralls:jupiter_test');
+	grunt.registerTask('jastest', 'jasmine_node:jupiter_test');
+	grunt.registerTask('coverload', 'coveralls:jupiter_test');
 };
