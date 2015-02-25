e1901e7c4fe05e359af2aaae445255b183e9ea0b
Matt Jordan <mbj755@gmail.com>
2015-02-25 17:30:14 -0500

    new refocroting

--- jupiter.js
+++ jupiter.js
@@ -21,67 +21,75 @@
     }
 }(this, function factory() {
     var topics = {};
-    var typeOf;
-    var jupiter;
 
-    typeOf = function typeOf(what) {
-        return Object.prototype.toString.call(what || null).replace(/\[object\s|\]/g, '').toLowerCase();
-    };
+    function typeOf(what, comparitor) {
+        return Object.prototype.toString.call(what || null)
+            .replace(/\[object\s|\]/g, '').toLowerCase() === comparitor;
+    }
 
-    jupiter = function jupiter(_context, topic) {
+    function getLength(arr) {
+        return arr.length;
+    }
 
-        function sub(key, fn, context) {
-            var newTopic = {};
+    function forEach(arr, fn) {
+        for (var i = 0; i < getLength(arr); i++) {
+            fn(arr[i], i, arr);
+        }
+    }
 
+    function jupiter(defaultContext, topic) {
+
+        function parseArguments(checkA, checkB, type, defaultVal) {
+            if (!!checkA && typeOf(checkA, type)){
+                return checkA;
+            }
+
+            if (!!checkB && typeOf(checkB, type)){
+                return checkB;
+            }
+
+            return defaultVal;
+        }
+
+        function generateNewTopicObject(key, fn, context) {
+            return {
+                'key': parseArguments(key, null, 'string', '_' + new Date().getTime()),
+                'fn': parseArguments(key, fn, 'function', function() {}),
+                'context': parseArguments(fn, context, 'object', defaultContext)
+            };
+        }
+
+        function sub() {
             if (!topics.hasOwnProperty(topic)) {
                 topics[topic] = [];
             }
-
-            newTopic.key = (typeOf(key) === 'string') ? key : '_' + new Date().getTime();
-            newTopic.fn = (typeOf(key) === 'function') ? key : ((!!fn && typeOf(fn) === 'function') ? fn : function() {});
-            newTopic.context = (!!fn && typeOf(fn) === 'object') ? fn : ((!!context) ? context : _context);
-
-            topics[topic].push(newTopic);
-
+            topics[topic].push(generateNewTopicObject.apply(this, arguments));
             return this;
         }
 
         function pub() {
-            var msg;
-            var i;
-            var len;
+            var args = arguments;
 
-            if (!topics.hasOwnProperty(topic)) {
-                return this;
-            }
-
-            msg = topics[topic];
-            len = msg.length;
-
-            for (i = 0; i < len; i++) {
-                msg[i].fn.apply(msg[i].context, arguments);
-            }
-
+            if (!topics.hasOwnProperty(topic)) return this;
+            forEach(topics[topic], function(val) {
+                val.fn.apply(val.context, args);
+            });
             return this;
         }
 
-        function unsub(key) {
-            var i;
-            var len;
+        function unsubWithKey(key) {
+            forEach(topics[topic], function(val, i) {
+                if (val.key === key) {
+                    topics[topic].splice(i, 1);
+                }
+            });
+        }
 
-            if (!topics.hasOwnProperty(topic)) {
-                return this;
-            }
+        function unsub(key) {
+            if (!topics.hasOwnProperty(topic)) return this;
 
             if (!!key) {
-                len = topics[topic].length;
-
-                for (i = 0; i < len; i++) {
-                    if (topics[topic][i].key === key) {
-                        topics[topic].splice(i, 1);
-                        break;
-                    }
-                }
+                unsubWithKey(key);
                 return this;
             }
 
@@ -89,45 +97,48 @@
             return this;
         }
 
-        function prove(fn, all) {
-            var _this = this;
-            var retValue = (!!all) ? topics : topics[topic];
-
-            if (!!fn && typeOf(fn) === 'function') {
-                fn.call(_this, retValue);
-            }
-            return _this;
+        function proveAllOrOne(all) {
+            return (!!all) ? topics : topics[topic];
         }
 
-        // Can easily alias methods here
+        function prove(fn, all) {
+            if (!!fn && typeOf(fn, 'function')) {
+                fn.call(this, proveAllOrOne(all));
+            }
+            return this;
+        }
+
+        // Define the public API
+        // Also useful for aliasing
         return {
             'sub': sub,
             'pub': pub,
             'unsub': unsub,
             'prove': prove
         };
-    };
+    }
 
-    return function(topic) {
+    function JupiterInstanceMultiTopic(topics) {
         var _this = this;
-        var multiTopic;
-        var i;
-        var len;
+        var multiTopicObj = {};
 
+        forEach(topics, function(val) {
+            multiTopicObj[val] = jupiter(_this, val);
+        });
+        return multiTopicObj;
+    }
+
+    function JupiterInstance(topic) {
         if (!topic) {
             throw new Error('Jupiter requires a topic');
         }
 
-        if (typeOf(topic) === 'array') {
-            multiTopic = {};
-            len = topic.length;
-
-            for (i = 0; i < len; i++) {
-                multiTopic[topic[i]] = jupiter(_this, topic[i]);
-            }
-            return multiTopic;
+        if (typeOf(topic, 'array')) {
+            return JupiterInstanceMultiTopic.call(this, topic);
         }
 
-        return jupiter(_this, topic);
-    };
+        return jupiter.call(this, topic);
+    }
+
+    return JupiterInstance;
 }));
